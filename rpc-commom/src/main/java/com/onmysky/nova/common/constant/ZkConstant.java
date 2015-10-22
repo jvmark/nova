3a69eb14ff3f0189c253b5cdd5ad18d6a94da831
mark <mark@duitang.com>
2015-10-22 15:04:33 +0800

    fix zookeeper dir

--- rpc-commom/src/main/java/com/onmysky/nova/common/constant/ZkConstant.java
+++ rpc-commom/src/main/java/com/onmysky/nova/common/constant/ZkConstant.java
@@ -6,6 +6,6 @@ package com.onmysky.nova.common.constant;
 public interface ZkConstant {
   int ZK_SESSION_TIMEOUT = 5000;
 
-  String ZK_REGISTRY_PATH = "/registry";
-  String ZK_DATA_PATH = ZK_REGISTRY_PATH + "/data";
+  String ZK_REGISTRY_PATH = "/zookeeper";
+  String ZK_DATA_PATH = ZK_REGISTRY_PATH + "/fuji";
 }
